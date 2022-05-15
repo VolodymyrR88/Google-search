4afe8df2784490205bc19b2225058dccb13983e4
Volodymyr Robotko <volodymyrrobotko@gmail.com>
2022-05-15 11:08:32 +0300

    changes

--- js/themes.js
+++ js/themes.js
@@ -1,16 +1,26 @@
 const themeL = document.getElementById("lightTheme");
 const themeD = document.getElementById("darkTheme");
+const themeDef = document.getElementById("defaultTheme");
 const bgTheme = document.getElementById("page");
 const setElem = document.getElementById("dropContent");
 
 themeL.onclick = function myBgL() {
-  bgTheme.classList.toggle("lightTheme");
-  bgTheme.classList.remove("darkTheme");
+  // bgTheme.classList.add("lightTheme");
+  // bgTheme.classList.remove("darkTheme");
+  // bgTheme.classList.remove("defaultTheme");
   setElem.classList.remove("show-dropContent");
 };
 
 themeD.onclick = function myBgD() {
-  bgTheme.classList.toggle("darkTheme");
-  bgTheme.classList.remove("lightTheme");
+  // bgTheme.classList.toggle("darkTheme");
+  // bgTheme.classList.remove("lightTheme");
+  // bgTheme.classList.remove("defaultTheme");
+  setElem.classList.remove("show-dropContent");
+};
+
+themeDef.onclick = function myBgDef() {
+  // bgTheme.classList.toggle("defaultThem");
+  // bgTheme.classList.remove("darkTheme");
+  // bgTheme.classList.remove("lightTheme");
   setElem.classList.remove("show-dropContent");
 };
