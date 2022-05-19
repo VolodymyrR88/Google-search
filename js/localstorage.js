331b8f07fcd670694f638c5adaf050cc4df26eb7
Volodymyr Robotko <volodymyrrobotko@gmail.com>
2022-05-19 18:41:26 +0300

    changes

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,23 +1,41 @@
 // ? theme ==========================================================
-const themeElements = document.querySelectorAll(".list-theme-item");
-const themeArray = [];
-const page = document.getElementById("page");
-// * ===================
+// const themeElements = document.querySelectorAll(".list-theme-item");
+// const themeArray = [];
+// const page = document.getElementById("page");
+// // * ===================
 
-// TODO: theme =========
-if (localStorage.getItem("theme") == null) {
-  page.classList.add("defaultTheme");
+// // TODO: theme =========
+// if (localStorage.getItem("theme") == null) {
+//   page.classList.add("defaultTheme");
+// } else {
+//   page.classList.add(localStorage.getItem("theme"));
+// }
+
+// themeElements.forEach((e) => {
+//   themeArray.push(e.getAttribute("data-color"));
+
+//   e.addEventListener("click", function () {
+//     page.classList.remove(...themeArray);
+//     page.classList.add(this.getAttribute("data-color"));
+//     localStorage.setItem("theme", this.getAttribute("data-color"));
+//   });
+// });
+// ! querySelectorAll================================================================
+let defaultTheme = localStorage.getItem("pageTheme");
+
+if (defaultTheme == null) {
+  $(".container").addClass("defaultTheme");
+  localStorage.setItem("pageTheme", "defaultTheme");
 } else {
-  page.classList.add(localStorage.getItem("theme"));
+  $(".container").addClass(localStorage.getItem("pageTheme"));
 }
 
-themeElements.forEach((e) => {
-  themeArray.push(e.getAttribute("data-color"));
-
-  e.addEventListener("click", function () {
-    page.classList.remove(...themeArray);
-    page.classList.add(this.getAttribute("data-color"));
-    localStorage.setItem("theme", this.getAttribute("data-color"));
-  });
+const button = $(".list-theme-item");
+button.on("click", function (element) {
+  let attributeClass = $(this).attr("data-color");
+  $(".container").removeClass("defaultTheme darkTheme lightTheme");
+  $(".container").addClass(attributeClass);
+  localStorage.setItem("pageTheme", attributeClass);
 });
-// ! ================================================================
+
+console.log(defaultTheme);
